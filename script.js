db3474d9aa6ae63434caab724c2fe5e1c558ea0c
ParrotBoi <149724975+ParrotBoi@users.noreply.github.com>
2024-12-17 16:55:34 -0500

    finished my music assignemtns

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+var catalogue = new Audio('catalogue.mp3');
 
+function Cat() {
+    catalogue.play();
+}
+
+
+var feminineAdornments = new Audio('feminine adornments.mp3');
+
+function FemAd() {
+    feminineAdornments.play();
+}
+
+
+var pianoInstrumental = new Audio('piano instrumental.mp3');
+
+function PianoIns() {
+    pianoInstrumental.play();
+}
+
+
+var stuckInACarWithAngels = new Audio('stuck in a car with angels.mp3');
+
+function SINACWA() {
+    stuckInACarWithAngels.play();
+}
+
+
+var veryLittleEffort = new Audio('very little effort.mp3');
+function VLE() {
+    veryLittleEffort.play();
+}
